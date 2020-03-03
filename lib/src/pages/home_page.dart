f59748888df8a0ed6d9dcbb510100da87b806e86
Baltazar Rodriguez Ramirez <baltazar@zubut.com>
2020-03-03 12:02:09 -0600

    Create HomePage and import CardSwiper plugin

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Movies in teathers'),
+        backgroundColor: Colors.indigoAccent,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () => {},
+          ),
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          _swiperCards(),
+        ],
+      ),
+    );
+  }
+
+  Widget _swiperCards() {
+    return Container(
+      padding: EdgeInsets.only(top: 16.0),
+      width: double.infinity,
+      height: 300.0,
+      child: Swiper(
+        itemBuilder: (BuildContext context,int index) {
+          return Image.network(
+            "http://via.placeholder.com/350x150",
+            fit: BoxFit.fill,
+          );
+        },
+        itemCount: 3,
+        itemWidth: 200.0,
+        //pagination: new SwiperPagination(),
+        //control: new SwiperControl(),
+        layout: SwiperLayout.STACK,
+      ),
+    );
+  }
+}
