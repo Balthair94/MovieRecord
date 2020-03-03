96608bb751196ff0a3108817c0f4c66883a0771d
Baltazar Rodriguez Ramirez <baltazar@zubut.com>
2020-03-03 13:42:03 -0600

    Create CardSwipeWidget

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:movie_record/src/widgets/card_swiper_widget.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -24,23 +24,8 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _swiperCards() {
-    return Container(
-      padding: EdgeInsets.only(top: 16.0),
-      width: double.infinity,
-      height: 300.0,
-      child: Swiper(
-        itemBuilder: (BuildContext context,int index) {
-          return Image.network(
-            "http://via.placeholder.com/350x150",
-            fit: BoxFit.fill,
-          );
-        },
-        itemCount: 3,
-        itemWidth: 200.0,
-        //pagination: new SwiperPagination(),
-        //control: new SwiperControl(),
-        layout: SwiperLayout.STACK,
-      ),
+    return CardSwiperWidget(
+      movies: [1,2,3,4,5],
     );
   }
 }
--- lib/src/widgets/card_swiper_widget.dart
+++ lib/src/widgets/card_swiper_widget.dart
@@ -1,8 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class CardSwiperWidget extends StatelessWidget {
+  final List<dynamic> movies;
+
+  CardSwiperWidget({this.movies});
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final Size _screenSize = MediaQuery.of(context).size;
+
+    return Container(
+      padding: EdgeInsets.only(top: 16.0),
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return ClipRRect(
+            borderRadius: BorderRadius.circular(24.0),
+            child: Image.network(
+              'https://i.picsum.photos/id/$index/500/300.jpg',
+              fit: BoxFit.fill,
+            ),
+          );
+        },
+        itemCount: movies.length,
+        itemWidth: _screenSize.width * 0.7,
+        itemHeight: _screenSize.height * 0.5,
+        layout: SwiperLayout.STACK,
+      ),
+    );
   }
 }
